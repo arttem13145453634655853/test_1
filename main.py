ecc53497fe1b8df975bd7e248b80c0281e581156
arttem13145453634655853 <a.a.anufriev@mail.ru>
2021-11-19 11:50:44 +0300

    add draw circle min blue

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import turtle
 turtle.speed(0)
 arr = list(map(int, input().split()))
 for i in range(10):
-    if arr[i] == max(arr):
-        turtle.pencolor('#ff0000')
-        turtle.circle(arr[i] * 10)
+    if arr[i] > 200:
+        turtle.pencolor('#0000ff')
+        turtle.circle(20)
         turtle.pencolor('#000000')
     else:
         turtle.circle(arr[i] * 10)
