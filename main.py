77f6ee3110d2b4f077734c3d1c7ffc11f4603885
arttem13145453634655853 <a.a.anufriev@mail.ru>
2021-11-19 11:49:08 +0300

    add draw circle red

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import turtle
 turtle.speed(0)
 arr = list(map(int, input().split()))
 for i in range(10):
-    turtle.circle(arr[i] * 10)
+    if arr[i] == max(arr):
+        turtle.pencolor('#ff0000')
+        turtle.circle(arr[i] * 10)
+        turtle.pencolor('#000000')
+    else:
+        turtle.circle(arr[i] * 10)
 turtle.mainloop()
-khkhyihliuyjg
